f2ee3ceedb73e93981fdba1612890fd6351d12d0
Manjit Bedi <dev@manjitbedi.com>
2024-08-24 14:57:08 -0700

    Initial Commit

---
+++ Hello-Godot/Hello_GodotApp.swift
@@ -0,0 +1,18 @@
+//
+//  Hello_GodotApp.swift
+//  Hello-Godot
+//
+//  Created by Manjit Bedi on 2024-08-24.
+//
+
+import SwiftUI
+
+@main
+struct Hello_GodotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
